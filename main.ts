21dc68dc52fd3383934a1fee0b300e6920fcf023
SKSMSSHWLDNSDLEK <135339588+SKSMSSHWLDNSDLEK@users.noreply.github.com>
2023-08-30 15:53:56 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,11 @@
+let _1 = 0
+input.onButtonPressed(Button.A, function () {
+    _1 = randint(2, 11)
+    basic.showNumber(_1)
+    if (true) {
+    	
+    }
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
